0fef8a15614ee32319c6a3d041467c7d593debc3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-09-28 05:40:15 +0300

    Added Program.cs.

---
+++ src/Web/SportStats.Web/Program.cs
@@ -0,0 +1,60 @@
+namespace SportStats.Web;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+using SportStats.Web.Data;
+using SportStats.Web.Services;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        CondifureServices(builder.Services, builder.Configuration);
+        var app = builder.Build();
+        Configure(app);
+        app.Run();
+    }
+
+    private static void CondifureServices(IServiceCollection services, IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
+        services.AddDatabaseDeveloperPageExceptionFilter();
+        services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddEntityFrameworkStores<ApplicationDbContext>();
+        services.AddRazorPages();
+
+        services.AddTransient<IUsersService, UsersService>();
+    }
+
+    private static void Configure(WebApplication app)
+    {
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+        app.UseCookiePolicy();
+
+        app.UseRouting();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute("areaRoute", "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+        app.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+    }
+}
