0d2a4172b62878e9705a10e21e6102aab8742cf0
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-11 20:49:51 +0300

    Modified function ConfigServices.

--- src/Apps/SportStats.Apps.ConsoleApp/Program.cs
+++ src/Apps/SportStats.Apps.ConsoleApp/Program.cs
@@ -10,12 +10,21 @@ public class Program
     public static async Task Main(string[] args)
     {
         var configuration = SetupConfiguration();
-        var services = ConfigServiceProvider(configuration);
+        var services = ConfigServices(configuration);
     }
 
-    private static ServiceProvider ConfigServiceProvider(IConfiguration configuration)
+    private static ServiceProvider ConfigServices(IConfiguration configuration)
     {
+        var services = new ServiceCollection();
 
+        // LOGGING
+        services.AddLogging(config =>
+        {
+
+        });
+
+        var serviceProvider = services.BuildServiceProvider();
+        return serviceProvider;
     }
 
     private static IConfiguration SetupConfiguration()
