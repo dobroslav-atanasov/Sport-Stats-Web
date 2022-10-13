22518be4a1b3e935957d464afc86296027a58c51
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-13 16:03:39 +0300

    Created db context.

---
+++ src/Data/SportStats.Data/Contexts/CountryDbContext.cs
@@ -0,0 +1,15 @@
+﻿namespace SportStats.Data.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+
+using SportStats.Data.Models.Entities.Countries;
+
+public class CountryDbContext : DbContext
+{
+    public CountryDbContext(DbContextOptions<CountryDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Country> Countries { get; set; }
+}
