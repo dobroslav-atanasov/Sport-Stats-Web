4c8e76e701df0bd9c52da3d21b863459ab75b2b1
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-13 16:00:18 +0300

    Created entities.

---
+++ src/Data/SportStats.Data.Models/Entities/Countries/Country.cs
@@ -0,0 +1,33 @@
+﻿namespace SportStats.Data.Models.Entities.Countries;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("Countries")]
+public class Country
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(10)]
+    public string Alpha2Code { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string NameUpperCase { get; set; }
+
+    [MaxLength(50)]
+    public string Continent { get; set; }
+
+    [MaxLength(200)]
+    public string Capital { get; set; }
+
+    public string Flag4x3 { get; set; }
+
+    public string Flag1x1 { get; set; }
+}
---
+++ src/Data/SportStats.Data.Models/Entities/CrawlerJsons/DataJson.cs
@@ -0,0 +1,20 @@
+﻿namespace SportStats.Data.Models.Entities.CrawlerJsons;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("DataJsons", Schema = "dbo")]
+public class DataJson
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Column(TypeName = "UNIQUEIDENTIFIER")]
+    public Guid Identifier { get; set; }
+
+    [Required]
+    public string Converter { get; set; }
+
+    [Required]
+    public string Json { get; set; }
+}
