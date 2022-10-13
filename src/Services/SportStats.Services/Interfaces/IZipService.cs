ae9487ecf85d1c1f76bfc7fd9291b9b25dd1668b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-13 16:22:34 +0300

    Created IZipService and implemented interfaces.

---
+++ src/Services/SportStats.Services/Interfaces/IZipService.cs
@@ -0,0 +1,11 @@
+﻿namespace SportStats.Services.Interfaces;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Data.Models.Zip;
+
+public interface IZipService
+{
+    byte[] ZipGroup(Group group);
+
+    List<ZipModel> UnzipGroup(byte[] data);
+}
---
+++ src/Services/SportStats.Services/ZipService.cs
@@ -0,0 +1,20 @@
+﻿namespace SportStats.Services;
+
+using System.Collections.Generic;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Data.Models.Zip;
+using SportStats.Services.Interfaces;
+
+public class ZipService : IZipService
+{
+    public List<ZipModel> UnzipGroup(byte[] data)
+    {
+        throw new NotImplementedException();
+    }
+
+    public byte[] ZipGroup(Group group)
+    {
+        throw new NotImplementedException();
+    }
+}
