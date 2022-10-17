92da0b2d53079550fdf5ba696da5bdd726ed47b3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-17 09:51:06 +0300

    Added constants.

--- src/Common/SportStats.Common.Constants/AppGlobalConstants.cs
+++ src/Common/SportStats.Common.Constants/AppGlobalConstants.cs
@@ -8,4 +8,9 @@ public static class AppGlobalConstants
     public const string LOG4NET_CORE = "Log4NetCore";
 
     public const string AUTOMAPPER_PROFILES_ASSEMBLY = "WorldSport.Services.Mapper";
+
+    public const string COUNTRY_CONNECTION_STRING = "CountryConnectionString";
+    public const string CRAWLER_NEW_CONNECTION_STRING = "CrawlerStorageNewConnectionString";
+    public const string CRALER_JSON_CONNECTION_STRING = "CrawlerStorageJSONConnectionString";
+    public const string OLYMPIC_GAMES_CONNETCTION_STRING = "OlympicGamesConnectionString";
 }
