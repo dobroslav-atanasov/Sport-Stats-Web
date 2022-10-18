1eae7aed226c0946866eed862f593e6d7f7df90f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-18 09:27:11 +0300

    Created model.

---
+++ src/Data/SportStats.Data.Common/Models/BaseDeletableModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Data.Common.Models;
+
+public abstract class BaseDeletableModel<TKey> : BaseModel<TKey>
+{
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+}
