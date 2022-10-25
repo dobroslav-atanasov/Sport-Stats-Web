08896f5cf5fd13fde5998ddabde873e9b5fa881f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-25 14:24:05 +0300

    Created mapper extension and model.

---
+++ src/Services/SportStats.Services.Mapper/Extensions/QueryableMapperExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace SportStats.Services.Mapper.Extensions;
+
+using System.Linq.Expressions;
+
+using AutoMapper.QueryableExtensions;
+
+public static class QueryableMapperExtensions
+{
+    public static IQueryable<TDestination> To<TDestination>(this IQueryable source, params Expression<Func<TDestination, object>>[] membersToExpand)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        return source.ProjectTo(MapperConfig.Mapper.ConfigurationProvider, null, membersToExpand);
+    }
+
+    public static IQueryable<TDestination> To<TDestination>(this IQueryable source, object parameters)
+    {
+        if (source == null)
+        {
+            throw new ArgumentNullException(nameof(source));
+        }
+
+        return source.ProjectTo<TDestination>(MapperConfig.Mapper.ConfigurationProvider, parameters);
+    }
+}
---
+++ src/Services/SportStats.Services.Mapper/Models/TypesMap.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Mapper.Models;
+
+public class TypesMap
+{
+    public Type Source { get; set; }
+
+    public Type Destination { get; set; }
+}
