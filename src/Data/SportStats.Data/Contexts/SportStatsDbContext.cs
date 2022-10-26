0029630b362d4f4a3fe7db7dc85f91f66384ae42
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-26 20:42:07 +0300

    Created db context.

---
+++ src/Data/SportStats.Data/Contexts/SportStatsDbContext.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Data.Contexts;
+
+using Microsoft.EntityFrameworkCore;
+
+public class SportStatsDbContext : DbContext
+{
+	public SportStatsDbContext(DbContextOptions<SportStatsDbContext> options)
+		: base(options)
+	{
+	}
+
+
+}
