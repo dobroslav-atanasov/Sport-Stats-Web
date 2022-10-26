c6059190e952acfd58f51666fabcf75fa23dc85f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-26 20:40:49 +0300

    Introduced service and interface HttpService.

---
+++ src/Services/SportStats.Services/HttpService.cs
@@ -0,0 +1,91 @@
+﻿namespace SportStats.Services;
+
+using System.Text;
+using System.Threading.Tasks;
+
+using HtmlAgilityPack;
+
+using SportStats.Data.Models.Http;
+using SportStats.Services.Interfaces;
+
+public class HttpService : IHttpService
+{
+    public async Task<byte[]> DownloadBytesAsync(string url)
+    {
+        using var handler = new HttpClientHandler();
+        using var client = new HttpClient(handler);
+
+        var response = await client.GetAsync(url);
+        var bytes = await response.Content.ReadAsByteArrayAsync();
+
+        return bytes;
+    }
+
+    public async Task<HttpModel> GetAsync(string url)
+    {
+        using var handler = new HttpClientHandler();
+        using var client = new HttpClient(handler);
+
+        var response = await client.GetAsync(url);
+        var responseAsString = await response.Content.ReadAsStringAsync();
+
+        while (responseAsString == "Rate Limit Exceeded")
+        {
+            await Task.Delay(3000);
+            response = await client.GetAsync(url);
+            responseAsString = await response.Content.ReadAsStringAsync();
+        }
+
+        if (response != null)
+        {
+            var httpModel = await this.CreateHttpModelAsync(response, url);
+            return httpModel;
+        }
+
+        return null;
+    }
+
+    public async Task<HttpModel> PostAsync(string url, string json)
+    {
+        using var handler = new HttpClientHandler();
+        using var client = new HttpClient(handler);
+
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+        var response = await client.PostAsync(url, content);
+
+        if (response != null)
+        {
+            var httpModel = await this.CreateHttpModelAsync(response, url);
+            return httpModel;
+        }
+
+        return null;
+    }
+
+    private async Task<HttpModel> CreateHttpModelAsync(HttpResponseMessage response, string url)
+    {
+        var httpModel = new HttpModel
+        {
+            Url = url,
+            StatusCode = response.StatusCode,
+            MimeType = response.Content.Headers.ContentType?.MediaType,
+            Content = await response.Content.ReadAsStringAsync(),
+        };
+
+        if (httpModel.MimeType != null && httpModel.MimeType == "text/html")
+        {
+            var htmlDocument = new HtmlDocument();
+            htmlDocument.LoadHtml(httpModel.Content);
+            httpModel.HtmlDocument = htmlDocument;
+        }
+
+        httpModel.Encoding = Encoding.UTF8;
+        var charSet = response.Content.Headers.ContentType?.CharSet;
+        if (charSet != null && charSet.ToLower() != "utf-8")
+        {
+            httpModel.Encoding = Encoding.GetEncoding(charSet);
+        }
+
+        return httpModel;
+    }
+}
---
+++ src/Services/SportStats.Services/Interfaces/IHttpService.cs
@@ -0,0 +1,12 @@
+﻿namespace SportStats.Services.Interfaces;
+
+using SportStats.Data.Models.Http;
+
+public interface IHttpService
+{
+    Task<HttpModel> GetAsync(string url);
+
+    Task<HttpModel> PostAsync(string url, string json);
+
+    Task<byte[]> DownloadBytesAsync(string url);
+}
