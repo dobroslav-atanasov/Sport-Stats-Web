930c64ac4c88911308e2032cf20a2477a5b3b3b6
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-27 09:21:08 +0300

    Added HttpModel.

---
+++ src/Data/SportStats.Data.Models/Http/HttpModel.cs
@@ -0,0 +1,25 @@
+﻿namespace SportStats.Data.Models.Http;
+
+using System.Net;
+using System.Text;
+
+using HtmlAgilityPack;
+
+public class HttpModel
+{
+    public string Url { get; set; }
+
+    public Uri Uri => new Uri(this.Url, UriKind.RelativeOrAbsolute);
+
+    public byte[] Bytes => this.Encoding.GetBytes(this.Content);
+
+    public Encoding Encoding { get; set; }
+
+    public string Content { get; set; }
+
+    public string MimeType { get; set; }
+
+    public HtmlDocument HtmlDocument { get; set; }
+
+    public HttpStatusCode StatusCode { get; set; }
+}
