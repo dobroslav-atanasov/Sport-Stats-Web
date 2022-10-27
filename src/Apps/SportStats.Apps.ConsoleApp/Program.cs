e3d6077416de0ea792a6910e9601a38097a13e11
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-27 16:19:40 +0300

    Registered services to the dependency container.

--- src/Apps/SportStats.Apps.ConsoleApp/Program.cs
+++ src/Apps/SportStats.Apps.ConsoleApp/Program.cs
@@ -1,14 +1,23 @@
 ﻿namespace SportStats.Apps.ConsoleApp;
 
+using System.Reflection;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using SportStats.Common.Constants;
+using SportStats.Common.Crawlers;
+using SportStats.Common.Crawlers.Countries;
 using SportStats.Data.Contexts;
+using SportStats.Data.Seeders;
+using SportStats.Data.Seeders.Interfaces;
 using SportStats.Services;
+using SportStats.Services.Data.CrawlerStorage;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
 using SportStats.Services.Interfaces;
+using SportStats.Services.Mapper;
 
 public class Program
 {
@@ -46,28 +55,41 @@ public class Program
         });
 
         // AUTOMAPPER
-        //MapperConfig.RegisterMapper(typeof().GetTypeInfo().Assembly);
+        MapperConfig.RegisterMapper(Assembly.Load(AppGlobalConstants.AUTOMAPPER_MODELS_ASSEMBLY));
 
         // DATABASES
-        var crawlerDbContextOptions = new DbContextOptionsBuilder<CrawlerDbContext>()
-            .UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.CRAWLER_NEW_CONNECTION_STRING))
+        var crawlerDbContextOptions = new DbContextOptionsBuilder<CrawlerStorageDbContext>()
+            .UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.CRAWLER_STORAGE_CONNECTION_STRING))
             .Options;
+
+        var crawlerStorageDbContext = new CrawlerStorageDbContext(crawlerDbContextOptions);
+        services.AddSingleton(crawlerStorageDbContext);
 
         var sportStatsDbContextOptions = new DbContextOptionsBuilder<SportStatsDbContext>()
             .UseSqlServer(configuration.GetConnectionString(AppGlobalConstants.SPORT_STATS_CONNECTION_STRING))
             .UseLazyLoadingProxies()
             .Options;
 
+        var sportStatsDbContext = new SportStatsDbContext(sportStatsDbContextOptions);
+        services.AddSingleton(sportStatsDbContext);
+
         // ENGINE
         services.AddScoped<Engine>();
 
-        // REPOSITORIES
+        // SEEDERS
+        services.AddTransient<ISeeder, CrawlerStorageSeeder>();
 
         // SERVICES
         services.AddScoped<IHttpService, HttpService>();
         services.AddScoped<IZipService, ZipService>();
 
+        // SERVICES DATA
+        services.AddScoped<ICrawlersService, CrawlersService>();
+        services.AddScoped<IOperationsService, OperationsService>();
+
         // CRAWLERS
+        services.AddTransient<CrawlerManager>();
+        services.AddTransient<WorldCountryCrawler>();
 
         // CONVERTERS
 
