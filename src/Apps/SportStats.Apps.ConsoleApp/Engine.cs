7ab950f252c41e62784d97b47d82686728cbc5ab
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-27 16:19:03 +0300

    Implemented part of the engine.

--- src/Apps/SportStats.Apps.ConsoleApp/Engine.cs
+++ src/Apps/SportStats.Apps.ConsoleApp/Engine.cs
@@ -1,9 +1,29 @@
 ﻿namespace SportStats.Apps.ConsoleApp;
 
+using SportStats.Common.Crawlers;
+using SportStats.Data.Seeders;
+using SportStats.Data.Seeders.Interfaces;
+
 internal class Engine
 {
+    private readonly CrawlerManager crawlerManager;
+    private readonly IEnumerable<ISeeder> seeders;
+
+    public Engine(CrawlerManager crawlerManager, IEnumerable<ISeeder> seeders)
+    {
+        this.crawlerManager = crawlerManager;
+        this.seeders = seeders;
+    }
+
     internal async Task RunAsync()
     {
+        // SEEDERS
+        var crawlerStorageSeeder = this.seeders.Single(s => s.SeederName == nameof(CrawlerStorageSeeder));
+        await crawlerStorageSeeder.SeedAsync();
 
+        // CRAWLERS
+        //await this.crawlerManager.RunWorldCountryCrawlers();
+
+        // CONVERTERS
     }
 }
