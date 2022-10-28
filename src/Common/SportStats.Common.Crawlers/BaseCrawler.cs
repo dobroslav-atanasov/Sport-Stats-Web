278816356de548c1e761fcc56da9bef154079a21
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-28 09:17:41 +0300

    Added field to base crawler.

--- src/Common/SportStats.Common.Crawlers/BaseCrawler.cs
+++ src/Common/SportStats.Common.Crawlers/BaseCrawler.cs
@@ -2,14 +2,20 @@
 
 using Microsoft.Extensions.Logging;
 
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
 using SportStats.Services.Interfaces;
 
 public abstract class BaseCrawler
 {
-	public BaseCrawler(ILogger<BaseCrawler> logger, IHttpService httpService)
+	private readonly ICrawlersService crawlersService;
+	private readonly Lazy<int> crawlerId;
+
+	public BaseCrawler(ILogger<BaseCrawler> logger, IHttpService httpService, ICrawlersService crawlersService)
 	{
 		this.Logger = logger;
 		this.HttpService = httpService;
+		this.crawlersService = crawlersService;
+		this.crawlerId = new Lazy<int>(() => this.crawlersService.GetCrawlerIdAsync(this.GetType().FullName).GetAwaiter().GetResult());
 	}
 
 	protected ILogger<BaseCrawler> Logger { get; }
