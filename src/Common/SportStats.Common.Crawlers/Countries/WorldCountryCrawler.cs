ca5ffdc40c6c01342cdb8dbf303935f3e724e448
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-28 09:50:42 +0300

    Created crawler.

---
+++ src/Common/SportStats.Common.Crawlers/Countries/WorldCountryCrawler.cs
@@ -0,0 +1,21 @@
+﻿namespace SportStats.Common.Crawlers.Countries;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class WorldCountryCrawler : BaseCrawler
+{
+    public WorldCountryCrawler(ILogger<BaseCrawler> logger, IHttpService httpService, ICrawlersService crawlersService)
+        : base(logger, httpService, crawlersService)
+    {
+    }
+
+    public override Task StartAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
