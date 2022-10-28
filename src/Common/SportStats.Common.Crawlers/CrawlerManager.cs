6787b655f23b4eeb7bc794500e9b17d1724a128f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-28 09:50:10 +0300

    Created crawler manager.

---
+++ src/Common/SportStats.Common.Crawlers/CrawlerManager.cs
@@ -0,0 +1,18 @@
+﻿namespace SportStats.Common.Crawlers;
+
+using SportStats.Common.Crawlers.Countries;
+
+public class CrawlerManager
+{
+	private readonly WorldCountryCrawler worldCountryCrawler;
+
+	public CrawlerManager(WorldCountryCrawler worldCountryCrawler)
+	{
+		this.worldCountryCrawler = worldCountryCrawler;
+	}
+
+	public async Task RunWorldCountryCrawlers()
+	{
+		await this.worldCountryCrawler.StartAsync();
+	}
+}
