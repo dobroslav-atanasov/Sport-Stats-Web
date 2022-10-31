035927982036f05ce3803f04187a6121527d81c0
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-31 15:01:34 +0200

    Added constants.

--- src/Common/SportStats.Common.Constants/CrawlerConstants.cs
+++ src/Common/SportStats.Common.Constants/CrawlerConstants.cs
@@ -2,7 +2,13 @@
 
 public static class CrawlerConstants
 {
-    public const string WORLD_COUNTRIES_URL = "https://flagpedia.net/index";
     public const string WORLD_COUNTRIES_MAIN_URL = "https://flagpedia.net";
+    public const string WORLD_COUNTRIES_URL = "https://flagpedia.net/index";
     public const string WORLD_COUNTRIES_DOWNLOAD_IMAGE = "/download/images";
+
+    public const string IOC_MAIN_URL = "https://olympics.com/";
+    public const string IOC_NOC_URL = "http://olympics.com/ioc/national-olympic-committees";
+
+    public const string OLYMPEDIA_MAIN_URL = "https://www.olympedia.org/";
+    public const string OLYMPEDIA_NOC_URL = "https://www.olympedia.org/countries";
 }
