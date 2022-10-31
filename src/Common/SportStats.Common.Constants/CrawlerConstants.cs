2271b04530e664c7721972b8bab9afc2da2d10ed
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-31 08:29:50 +0200

    Created file with constants.

---
+++ src/Common/SportStats.Common.Constants/CrawlerConstants.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Common.Constants;
+
+public static class CrawlerConstants
+{
+    public const string WORLD_COUNTRIES_URL = "https://flagpedia.net/index";
+    public const string WORLD_COUNTRIES_MAIN_URL = "https://flagpedia.net";
+    public const string WORLD_COUNTRIES_DOWNLOAD_IMAGE = "/download/images";
+}
