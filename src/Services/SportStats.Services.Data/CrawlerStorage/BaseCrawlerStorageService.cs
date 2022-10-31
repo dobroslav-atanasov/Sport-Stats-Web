94f0b6261217ad6754c43d4c87d3bd6ab9100392
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-10-31 08:31:48 +0200

    Created base data service.

---
+++ src/Services/SportStats.Services.Data/CrawlerStorage/BaseCrawlerStorageService.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Services.Data.CrawlerStorage;
+
+using SportStats.Data.Contexts;
+
+public abstract class BaseCrawlerStorageService
+{
+	public BaseCrawlerStorageService(CrawlerStorageDbContext context)
+	{
+		this.Context = context;
+	}
+
+	protected CrawlerStorageDbContext Context { get; }
+}
