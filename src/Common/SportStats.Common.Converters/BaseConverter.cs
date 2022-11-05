df4059ac748e0af3d93fc15a7befe10ff9755814
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-05 20:30:38 +0200

    Implemented base converter.

--- src/Common/SportStats.Common.Converters/BaseConverter.cs
+++ src/Common/SportStats.Common.Converters/BaseConverter.cs
@@ -1,56 +1,76 @@
 ﻿namespace SportStats.Common.Converters;
 
+using Dasync.Collections;
+
 using Microsoft.Extensions.Logging;
 
 using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Interfaces;
 
 public abstract class BaseConverter
 {
-    public BaseConverter(ILogger<BaseConverter> logger)
+    private readonly ICrawlersService crawlersService;
+    private readonly ILogsService logsService;
+    private readonly IGroupsService groupsService;
+    private readonly IZipService zipService;
+
+    public BaseConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService)
     {
         this.Logger = logger;
+        this.crawlersService = crawlersService;
+        this.logsService = logsService;
+        this.groupsService = groupsService;
+        this.zipService = zipService;
     }
 
     protected ILogger<BaseConverter> Logger { get; }
 
     protected abstract Task ProcessGroupAsync(Group group);
 
-    public async Task ConvertAsync(string converterName)
+    public async Task ConvertAsync(string crawlerName)
     {
-        //this.Logger.LogInformation($"Converter: {converterName} start.");
+        this.Logger.LogInformation($"Converter: {crawlerName} start.");
 
-        //try
-        //{
-        //    // CONVERT ONLY 1 GUID
-        //    //var groupDto = await this.ConverterService.GetGroupAsync(Guid.Parse("FF0E2B2A-45F9-4E39-9744-C65D9CA5AE0B"));
-        //    //await this.ProcessGroupAsync(groupDto);
+        try
+        {
+            // CONVERT ONLY 1 GUID
+            //var groupDto = await this.groupsService.GetGroupAsync(Guid.Parse("FF0E2B2A-45F9-4E39-9744-C65D9CA5AE0B"));
+            //await this.ProcessGroupAsync(groupDto);
 
-        //    var crawler = await this.ConverterService.GetCrawlerAsync(converterName);
-        //    var identifiers = await this.ConverterService.GetIdentifiersAsync(crawler.Id);
+            var crawlerId = await this.crawlersService.GetCrawlerIdAsync(crawlerName);
+            var identifiers = await this.logsService.GetLogIdentifiersAsync(crawlerId);
 
-        //    //identifiers = new List<Guid>
-        //    //{
-        //    //    Guid.Parse("FD8B285D-C165-4407-9019-871464D17A02")
-        //    //};
+            //identifiers = new List<Guid>
+            //{
+            //    Guid.Parse("FD8B285D-C165-4407-9019-871464D17A02")
+            //};
 
-        //    await identifiers.ParallelForEachAsync(async identifier =>
-        //    {
-        //        try
-        //        {
-        //            var group = await this.ConverterService.GetGroupAsync(identifier);
-        //            await this.ProcessGroupAsync(group);
-        //        }
-        //        catch (Exception ex)
-        //        {
-        //            this.Logger.LogError(ex, $"Group was not process: {identifier};");
-        //        }
-        //    }, maxDegreeOfParallelism: 1);
-        //}
-        //catch (Exception ex)
-        //{
-        //    this.Logger.LogError(ex, $"Failed to process documents from converter: {converterName};");
-        //}
+            await identifiers.ParallelForEachAsync(async identifier =>
+            {
+                try
+                {
+                    var group = await this.groupsService.GetGroupAsync(identifier);
+                    var zipModels = this.zipService.UnzipGroup(group.Content);
+                    foreach (var document in group.Documents)
+                    {
+                        var zipModel = zipModels.First(z => z.Name == document.Name);
+                        document.Content = zipModel.Content;
+                    }
 
-        //this.Logger.LogInformation($"Converter: {converterName} end.");
+                    await this.ProcessGroupAsync(group);
+                }
+                catch (Exception ex)
+                {
+                    this.Logger.LogError(ex, $"Group was not process: {identifier};");
+                }
+            }, maxDegreeOfParallelism: 1);
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process documents from converter: {crawlerName};");
+        }
+
+        this.Logger.LogInformation($"Converter: {crawlerName} end.");
     }
 }
