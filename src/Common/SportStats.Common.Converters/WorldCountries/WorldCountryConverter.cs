40e1d6e170fcdc07c49924730a98ee4b0e8e44c3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:22:17 +0200

    First converter was created.

---
+++ src/Common/SportStats.Common.Converters/WorldCountries/WorldCountryConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace SportStats.Common.Converters.WorldCountries;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Data.Models.Entities.SportStats;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Data.SportStats.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class WorldCountryConverter : BaseConverter
+{
+    private readonly ICountryService countryService;
+
+    public WorldCountryConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        ICountryService countryService)
+        : base(logger, crawlersService, logsService, groupsService, zipService)
+    {
+        this.countryService = countryService;
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        var document = this.CreateHtmlDocument(group.Documents.Single());
+        var header = document
+            .DocumentNode
+            .SelectSingleNode("//h1")
+            .InnerText;
+
+        var country = new Country();
+
+        //await this.countryService.AddAsync(country);
+    }
+}
