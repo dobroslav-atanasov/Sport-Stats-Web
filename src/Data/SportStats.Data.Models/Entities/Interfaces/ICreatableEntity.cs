f25324d5ea9fee4434fc3e592e1f4074e5010c5d
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:23:29 +0200

    Added interfaces.

---
+++ src/Data/SportStats.Data.Models/Entities/Interfaces/ICreatableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Data.Models.Entities.Interfaces;
+
+public interface ICreatableEntity
+{
+    DateTime CreatedOn { get; }
+
+    DateTime? ModifiedOn { get; set; }
+}
---
+++ src/Data/SportStats.Data.Models/Entities/Interfaces/IDeletableEntity.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Data.Models.Entities.Interfaces;
+
+public interface IDeletableEntity
+{
+    bool IsDeleted { get; set; }
+
+    DateTime? DeletedOn { get; set; }
+}
