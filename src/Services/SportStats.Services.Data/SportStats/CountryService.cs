8d60b8b3cdae407f07b98b9594bc0754cd22a371
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:24:37 +0200

    Introduced country service.

---
+++ src/Services/SportStats.Services.Data/SportStats/CountryService.cs
@@ -0,0 +1,33 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class CountryService : BaseSportStatsService, ICountryService
+{
+    public CountryService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<Country> AddAsync(Country country)
+    {
+        await this.Context.AddAsync(country);
+        await this.Context.SaveChangesAsync();
+
+        return country;
+    }
+
+    public async Task<Country> UpdateAsync(Country country)
+    {
+        this.Context.Entry(country).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return country;
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/ICountryService.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface ICountryService
+{
+    Task<Country> AddAsync(Country country);
+
+    Task<Country> UpdateAsync(Country country);
+}
