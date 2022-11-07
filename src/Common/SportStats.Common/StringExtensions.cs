625124bfaa11b4fb71767bcff92a2b89e898c862
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:19:32 +0200

    Created extension file.

---
+++ src/Common/SportStats.Common/StringExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace SportStats.Common;
+
+using System.Net;
+
+public static class StringExtensions
+{
+    public static string Decode(this string text)
+    {
+        return WebUtility.HtmlDecode(text);
+    }
+}
