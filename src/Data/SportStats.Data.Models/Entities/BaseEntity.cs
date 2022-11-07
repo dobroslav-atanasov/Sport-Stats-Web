8bdca8872d7928355c5493b69ab043304eb001c8
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:23:06 +0200

    Base entity with id was created.

---
+++ src/Data/SportStats.Data.Models/Entities/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace SportStats.Data.Models.Entities;
+
+using System.ComponentModel.DataAnnotations;
+
+public abstract class BaseEntity<TKey>
+{
+    [Key]
+    public TKey Id { get; set; }
+}
