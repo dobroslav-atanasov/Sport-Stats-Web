d66215505c292c23afab020e1dd15e410fbc37f6
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:25:07 +0200

    Created base service abstract class.

---
+++ src/Services/SportStats.Services.Data/SportStats/BaseSportStatsService.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using global::SportStats.Data.Contexts;
+
+public abstract class BaseSportStatsService
+{
+	public BaseSportStatsService(SportStatsDbContext context)
+	{
+		this.Context = context;
+	}
+
+	protected SportStatsDbContext Context { get; }
+}
