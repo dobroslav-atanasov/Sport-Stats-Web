a95d4c2f77a8c86e968f759ec4bf119bbddf1d71
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:20:02 +0200

    Added ConverterConstants.cs file.

---
+++ src/Common/SportStats.Common.Constants/ConverterConstants.cs
@@ -0,0 +1,6 @@
+﻿namespace SportStats.Common.Constants;
+
+public static class ConverterConstants
+{
+    public const string WORLD_COUNTRIES_MAIN_URL = "SportStats.Common.Crawlers.Countries.WorldCountryCrawler";
+}
