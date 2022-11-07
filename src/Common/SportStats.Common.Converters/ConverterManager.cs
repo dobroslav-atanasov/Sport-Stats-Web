ab9ea4a529a7612535b9d06a65cba4b96a4b2815
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-07 08:20:33 +0200

    Introduced converter manager.

---
+++ src/Common/SportStats.Common.Converters/ConverterManager.cs
@@ -0,0 +1,19 @@
+﻿namespace SportStats.Common.Converters;
+
+using SportStats.Common.Constants;
+using SportStats.Common.Converters.WorldCountries;
+
+public class ConverterManager
+{
+	private readonly WorldCountryConverter worldCountryConverter;
+
+	public ConverterManager(WorldCountryConverter worldCountryConverter)
+	{
+		this.worldCountryConverter = worldCountryConverter;
+	}
+
+	public async Task RunWorldCountriesConverters()
+	{
+		await this.worldCountryConverter.ConvertAsync(ConverterConstants.WORLD_COUNTRIES_MAIN_URL);
+	}
+}
