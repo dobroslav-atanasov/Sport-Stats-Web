82f82139e8e2fbcba0e71f280c3bd42e64f11517
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-08 08:34:55 +0200

    Implemented part of the service.

--- src/Services/SportStats.Services/Interfaces/IRegexService.cs
+++ src/Services/SportStats.Services/Interfaces/IRegexService.cs
@@ -1,5 +1,12 @@
 ﻿namespace SportStats.Services.Interfaces;
 
+using System.Text.RegularExpressions;
+
 public interface IRegexService
 {
+    string MatchFirstGroup(string text, string pattern);
+
+    Match Match(string text, string pattern);
+
+    string Replace(string text, string pattern, string replacement);
 }
--- src/Services/SportStats.Services/RegexService.cs
+++ src/Services/SportStats.Services/RegexService.cs
@@ -1,7 +1,37 @@
 ﻿namespace SportStats.Services;
 
+using System.Text.RegularExpressions;
+
+using SportStats.Common;
 using SportStats.Services.Interfaces;
 
 public class RegexService : IRegexService
 {
+    public Match Match(string text, string pattern)
+    {
+        var match = Regex.Match(text, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        if (match.Success)
+        {
+            return match;
+        }
+
+        return null;
+    }
+
+    public string MatchFirstGroup(string text, string pattern)
+    {
+        var match = Regex.Match(text, pattern, RegexOptions.IgnoreCase | RegexOptions.Singleline);
+        if (match.Success)
+        {
+            var result = match.Groups[1].Value.Trim().Decode();
+            return result;
+        }
+
+        return null;
+    }
+
+    public string Replace(string text, string pattern, string replacement)
+    {
+        return Regex.Replace(text, pattern, replacement);
+    }
 }
