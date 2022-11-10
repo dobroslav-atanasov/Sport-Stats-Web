02e232c4ac7a728bbaac3f47928c47bbb1360b24
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-10 14:34:44 +0200

    Created converter.

---
+++ src/Common/SportStats.Common.Converters/Olympedia/NOCConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace SportStats.Common.Converters.Olympedia;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class NOCConverter : BaseConverter
+{
+    public NOCConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService)
+        : base(logger, crawlersService, logsService, groupsService, zipService)
+    {
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        ;
+    }
+}
