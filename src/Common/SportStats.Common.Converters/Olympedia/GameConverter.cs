a9b803aa1334b5e1f17c14b137869cd6be0090dc
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-12 15:06:35 +0200

    The GameConverter was added.

---
+++ src/Common/SportStats.Common.Converters/Olympedia/GameConverter.cs
@@ -0,0 +1,22 @@
+﻿namespace SportStats.Common.Converters.Olympedia;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class GameConverter : BaseConverter
+{
+    public GameConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService)
+        : base(logger, crawlersService, logsService, groupsService, zipService)
+    {
+    }
+
+    protected override Task ProcessGroupAsync(Group group)
+    {
+        throw new NotImplementedException();
+    }
+}
