f86d32f8306cc1e0f22ecfce92191c743e042163
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-13 20:14:26 +0200

    DataCacheService was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/DataCacheService.cs
@@ -0,0 +1,18 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using global::SportStats.Data.Models.Cache;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+public class DataCacheService : IDataCacheService
+{
+    private readonly Lazy<ICollection<OGCountryCacheModel>> ogCountryCacheModels;
+    private readonly ICountriesService countriesService;
+
+    public DataCacheService(ICountriesService countriesService)
+    {
+        this.countriesService = countriesService;
+        this.ogCountryCacheModels = new Lazy<ICollection<OGCountryCacheModel>>(() => this.countriesService.GetOGCountriesCache());
+    }
+
+    public ICollection<OGCountryCacheModel> OGCacheCountries => this.ogCountryCacheModels.Value;
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IDataCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Cache;
+
+public interface IDataCacheService
+{
+    ICollection<OGCountryCacheModel> OGCacheCountries { get; }
+}
