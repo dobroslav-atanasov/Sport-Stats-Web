128f865fff9a0e4bec6f11c96551712db9d4fdfc
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-14 14:31:53 +0200

    GetMonthNumber function is created.

--- src/Common/SportStats.Common/StringExtensions.cs
+++ src/Common/SportStats.Common/StringExtensions.cs
@@ -8,4 +8,50 @@ public static class StringExtensions
     {
         return WebUtility.HtmlDecode(text);
     }
+
+    public static int GetMonthNumber(this string text)
+    {
+        var month = 0;
+
+        switch (text.ToLower())
+        {
+            case "january":
+            case "jan":
+                month = 1; break;
+            case "february":
+            case "feb":
+                month = 2; break;
+            case "march":
+            case "mar":
+                month = 3; break;
+            case "april":
+            case "apr":
+                month = 4; break;
+            case "may":
+                month = 5; break;
+            case "june":
+            case "jun":
+                month = 6; break;
+            case "july":
+            case "jul":
+                month = 7; break;
+            case "august":
+            case "aug":
+                month = 8; break;
+            case "september":
+            case "sep":
+                month = 9; break;
+            case "october":
+            case "oct":
+                month = 10; break;
+            case "november":
+            case "nov":
+                month = 11; break;
+            case "december":
+            case "dec":
+                month = 12; break;
+        }
+
+        return month;
+    }
 }
