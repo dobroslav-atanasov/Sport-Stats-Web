ed56e711a2d73fca707247c6a73e98a53d8ff449
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-14 08:42:22 +0200

    The is updated.

--- src/Common/SportStats.Common.Constants/ConverterConstants.cs
+++ src/Common/SportStats.Common.Constants/ConverterConstants.cs
@@ -5,4 +5,5 @@ public static class ConverterConstants
     public const string WORLD_COUNTRIES_CONVERTER = "SportStats.Common.Crawlers.Countries.WorldCountryCrawler";
 
     public const string OLYMPEDIA_NOC_CONVERTER = "SportStats.Common.Crawlers.Olympedia.NOCCrawler";
+    public const string OLYMPEDIA_GAME_CONVERTER = "SportStats.Common.Crawlers.Olympedia.GameCrawler";
 }
