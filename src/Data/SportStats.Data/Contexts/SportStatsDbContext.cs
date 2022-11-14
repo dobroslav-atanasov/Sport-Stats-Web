76c6e62b5b81523b4ef16daca13aed80890ea69e
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-14 14:37:39 +0200

    In the context was added db sets.

--- src/Data/SportStats.Data/Contexts/SportStatsDbContext.cs
+++ src/Data/SportStats.Data/Contexts/SportStatsDbContext.cs
@@ -3,10 +3,18 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using SportStats.Data.Models.Entities.SportStats;
+
 public class SportStatsDbContext : IdentityDbContext
 {
     public SportStatsDbContext(DbContextOptions<SportStatsDbContext> options)
         : base(options)
     {
     }
+
+    public virtual DbSet<WorldCountry> Countries { get; set; }
+
+    public virtual DbSet<OGCountry> OGCountries { get; set; }
+
+    public virtual DbSet<OGGame> OGGames { get; set; }
 }
