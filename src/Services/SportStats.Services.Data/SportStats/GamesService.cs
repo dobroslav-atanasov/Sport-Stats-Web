b701d76be19e446ab5c13f7ae8132d948ce72c18
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-14 14:29:28 +0200

    The GamesService was created.

---
+++ src/Services/SportStats.Services.Data/SportStats/GamesService.cs
@@ -0,0 +1,43 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Data.Models.Enumerations;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class GamesService : BaseSportStatsService, IGamesService
+{
+    public GamesService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<OGGame> AddAsync(OGGame game)
+    {
+        await this.Context.AddAsync(game);
+        await this.Context.SaveChangesAsync();
+
+        return game;
+    }
+
+    public async Task<OGGame> GetGameAsync(int year, OlympicGameType type)
+    {
+        var game = await this.Context
+            .OGGames
+            .FirstOrDefaultAsync(g => g.Year == year && g.Type == type);
+
+        return game;
+    }
+
+    public async Task<OGGame> UpdateAsync(OGGame game)
+    {
+        this.Context.Entry(game).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return game;
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IGamesService.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Data.Models.Enumerations;
+
+public interface IGamesService
+{
+    Task<OGGame> GetGameAsync(int year, OlympicGameType type);
+
+    Task<OGGame> AddAsync(OGGame game);
+
+    Task<OGGame> UpdateAsync(OGGame game);
+}
