ab2135d3ebf1507c10f385913c674e756fd98d06
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-15 10:50:13 +0200

    SportsService is added.

---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/ISportsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface ISportsService : IAddable, IUpdatable
+{
+    Task<OGSport> GetSportAsync(string name);
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/SportsService.cs
@@ -0,0 +1,38 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class SportsService : BaseSportStatsService, ISportsService
+{
+    public SportsService(SportStatsDbContext context) 
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public Task<OGSport> GetSportAsync(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        this.Context.Entry(entity).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
