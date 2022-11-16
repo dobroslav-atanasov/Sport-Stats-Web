a00e0dd8b08495018d4e80f41fb540c88313c9c1
dobroslav-atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-16 15:22:41 +0200

    SportDisciplineConverter was created.

---
+++ src/Common/SportStats.Common.Converters/Olympedia/SportDisciplineConverter.cs
@@ -0,0 +1,33 @@
+﻿namespace SportStats.Common.Converters.Olympedia;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class SportDisciplineConverter : BaseConverter
+{
+    public SportDisciplineConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, 
+        IZipService zipService, IRegexService regexService) 
+        : base(logger, crawlersService, logsService, groupsService, zipService, regexService)
+    {
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        try
+        {
+            var document = this.CreateHtmlDocument(group.Documents.Single());
+            var lines = document.DocumentNode.SelectNodes("//table[@class='table table-striped sortable']//tr");
+
+            ;
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
+        }
+    }
+}
