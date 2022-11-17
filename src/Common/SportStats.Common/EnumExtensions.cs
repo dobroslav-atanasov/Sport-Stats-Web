5109bf2d4c4716a7be29c3ab41f046e0ca7fcc95
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-17 08:50:11 +0200

    New extension file was added.

---
+++ src/Common/SportStats.Common/EnumExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace SportStats.Common;
+
+public static class EnumExtensions
+{
+    public static T ToEnum<T>(this string value, bool ignoreCase = true)
+    {
+        return (T)Enum.Parse(typeof(T), value, ignoreCase);
+    }
+}
