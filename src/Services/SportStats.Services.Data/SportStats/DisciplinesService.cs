664995dbad80830825d8b4f91585c9416c6fb882
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-17 08:51:13 +0200

    The DisciplinesService was introduced.

---
+++ src/Services/SportStats.Services.Data/SportStats/DisciplinesService.cs
@@ -0,0 +1,42 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class DisciplinesService : BaseSportStatsService, IDisciplinesService
+{
+    public DisciplinesService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<OGDiscipline> GetDisciplineAsync(string name)
+    {
+        var discipline = await this.Context
+            .OGDisciplines
+            .FirstOrDefaultAsync(d => d.Name == name);
+
+        return discipline;
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        this.Context.Entry(entity).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IDisciplinesService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface IDisciplinesService : IAddable, IUpdatable
+{
+    Task<OGDiscipline> GetDisciplineAsync(string name);
+}
