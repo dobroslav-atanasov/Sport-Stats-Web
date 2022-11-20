d512933a6564a5cdfa086277de6f0fca784f7d62
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-20 13:56:22 +0200

    EventsService was introduced.

---
+++ src/Services/SportStats.Services.Data/SportStats/EventsService.cs
@@ -0,0 +1,42 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class EventsService : BaseSportStatsService, IEventsService
+{
+    public EventsService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<OGEvent> GetEventAsync(string name, int disciplineId, int gameId)
+    {
+        var @event = await this.Context
+            .OGEvents
+            .FirstOrDefaultAsync(e => e.Name == name && e.DisciplineId == disciplineId && e.GameId == gameId);
+
+        return @event;
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        this.Context.Entry(entity).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IEventsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface IEventsService : IAddable, IUpdatable
+{
+    Task<OGEvent> GetEventAsync(string name, int disciplineId, int gameId);
+}
