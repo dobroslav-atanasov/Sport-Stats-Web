5149dc5f202c85d4120e3d46291fbf2d7540e55b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-21 08:01:38 +0200

    Cache models were added.

---
+++ src/Data/SportStats.Data.Models/Cache/OGDisciplineCacheModel.cs
@@ -0,0 +1,11 @@
+﻿namespace SportStats.Data.Models.Cache;
+
+using SportStats.Data.Models.Entities.SportStats;
+using SportStats.Services.Mapper.Interfaces;
+
+public class OGDisciplineCacheModel : IMapFrom<OGDiscipline>
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+}
---
+++ src/Data/SportStats.Data.Models/Cache/OGGameCacheModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SportStats.Data.Models.Cache;
+
+using SportStats.Data.Models.Entities.SportStats;
+using SportStats.Data.Models.Enumerations;
+using SportStats.Services.Mapper.Interfaces;
+
+public class OGGameCacheModel : IMapFrom<OGGame>
+{
+    public int Id { get; set; }
+
+    public int Year { get; set; }
+
+    public OlympicGameType Type { get; set; }
+}
