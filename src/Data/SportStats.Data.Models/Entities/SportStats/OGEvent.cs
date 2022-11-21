f6e20da9400b709bb6cac7920b40cc38dd488baf
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-21 08:02:33 +0200

    An entities were added.

---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGEvent.cs
@@ -0,0 +1,107 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportStats.Data.Models.Entities.Interfaces;
+
+[Table("OlympicGames_Events", Schema = "dbo")]
+public class OGEvent : BaseEntity<int>, ICreatableEntity, IDeletableEntity, IUpdatable<OGEvent>
+{
+    [Required]
+    [MaxLength(200)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(200)]
+    public string NormalizedName { get; set; }
+
+    public int DisciplineId { get; set; }
+    public virtual OGDiscipline Discipline { get; set; }
+
+    public int GameId { get; set; }
+    public virtual OGGame Game { get; set; }
+
+    public DateTime? StartDate { get; set; }
+
+    public DateTime? EndDate { get; set; }
+
+    [Required]
+    public bool IsTeamEvent { get; set; } = false;
+
+    [MaxLength(200)]
+    public string AdditionalInfo { get; set; }
+
+    public int Athletes { get; set; }
+
+    public int Countries { get; set; }
+
+    [MaxLength(1000)]
+    public string Format { get; set; }
+
+    public string Description { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public bool Update(OGEvent other)
+    {
+        var isUpdated = false;
+
+        if (this.StartDate != other.StartDate)
+        {
+            this.StartDate = other.StartDate;
+            isUpdated = true;
+        }
+
+        if (this.EndDate != other.EndDate)
+        {
+            this.EndDate = other.EndDate;
+            isUpdated = true;
+        }
+
+        if (this.IsTeamEvent != other.IsTeamEvent)
+        {
+            this.IsTeamEvent = other.IsTeamEvent;
+            isUpdated = true;
+        }
+
+        if (this.AdditionalInfo != other.AdditionalInfo)
+        {
+            this.AdditionalInfo = other.AdditionalInfo;
+            isUpdated = true;
+        }
+
+        if (this.Athletes != other.Athletes)
+        {
+            this.Athletes = other.Athletes;
+            isUpdated = true;
+        }
+
+        if (this.Countries != other.Countries)
+        {
+            this.Countries = other.Countries;
+            isUpdated = true;
+        }
+
+        if (this.Format != other.Format)
+        {
+            this.Format = other.Format;
+            isUpdated = true;
+        }
+
+        if (this.Description != other.Description)
+        {
+            this.Description = other.Description;
+            isUpdated = true;
+        }
+
+        return isUpdated;
+    }
+}
---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGVenue.cs
@@ -0,0 +1,107 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportStats.Data.Models.Entities.Interfaces;
+
+[Table("OlympicGames_Venues", Schema = "dbo")]
+public class OGVenue : BaseEntity<int>, ICreatableEntity, IDeletableEntity, IUpdatable<OGVenue>
+{
+    [Required]
+    public int Number { get; set; }
+
+    [Required]
+    [MaxLength(500)]
+    public string Name { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public string City { get; set; }
+
+    [MaxLength(500)]
+    public string EnglishName { get; set; }
+
+    [MaxLength(1000)]
+    public string FullName { get; set; }
+
+    public double? LatitudeCoordinate { get; set; }
+
+    public double? LongitudeCoordinate { get; set; }
+
+    public int? Opened { get; set; }
+
+    public int? Demolished { get; set; }
+
+    public string Capacity { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public bool Update(OGVenue other)
+    {
+        var isUpdated = false;
+
+        if (this.Name != other.Name)
+        {
+            this.Name = other.Name;
+            isUpdated = true;
+        }
+
+        if (this.City != other.City)
+        {
+            this.City = other.City;
+            isUpdated = true;
+        }
+
+        if (this.EnglishName != other.EnglishName)
+        {
+            this.EnglishName = other.EnglishName;
+            isUpdated = true;
+        }
+
+        if (this.FullName != other.FullName)
+        {
+            this.FullName = other.FullName;
+            isUpdated = true;
+        }
+
+        if (this.LatitudeCoordinate != other.LatitudeCoordinate)
+        {
+            this.LatitudeCoordinate = other.LatitudeCoordinate;
+            isUpdated = true;
+        }
+
+        if (this.LongitudeCoordinate != other.LongitudeCoordinate)
+        {
+            this.LongitudeCoordinate = other.LongitudeCoordinate;
+            isUpdated = true;
+        }
+
+        if (this.Opened != other.Opened)
+        {
+            this.Opened = other.Opened;
+            isUpdated = true;
+        }
+
+        if (this.Demolished != other.Demolished)
+        {
+            this.Demolished = other.Demolished;
+            isUpdated = true;
+        }
+
+        if (this.Capacity != other.Capacity)
+        {
+            this.Capacity = other.Capacity;
+            isUpdated = true;
+        }
+
+        return isUpdated;
+    }
+}
