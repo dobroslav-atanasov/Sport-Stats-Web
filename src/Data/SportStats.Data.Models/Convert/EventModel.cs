eeef00edb2d5d4ada30ae0b4050f31f32f29c66d
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-21 08:03:13 +0200

    Additional model was added.

---
+++ src/Data/SportStats.Data.Models/Convert/EventModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SportStats.Data.Models.Convert;
+
+public class EventModel
+{
+    public string Name { get; set; }
+
+    public string OriginalName { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public string AdditionalInfo { get; set; }
+}
