6a628a3daeac8133379c643c18c9eaf2a82d9fb9
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-21 08:00:43 +0200

    An extensions were created.

---
+++ src/Common/SportStats.Common/DoubleExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace SportStats.Common;
+
+public static class DoubleExtensions
+{
+    private const double POUND = 0.45359237;
+
+    public static double ConvertPoundToKilograms(this double value)
+    {
+        return value * POUND;
+    }
+}
