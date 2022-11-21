cf6dfe3a8d0a4b9e43fa2370f089e8cdff1855f3
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-21 08:03:45 +0200

    The venues service was introduced.

---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IVenuesService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface IVenuesService : IAddable, IUpdatable
+{
+    Task<OGVenue> GetVenueAsync(int number);
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/VenuesService.cs
@@ -0,0 +1,42 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class VenuesService : BaseSportStatsService, IVenuesService
+{
+    public VenuesService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<OGVenue> GetVenueAsync(int number)
+    {
+        var venue = await this.Context
+            .OGVenues
+            .FirstOrDefaultAsync(v => v.Number == number);
+
+        return venue;
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        this.Context.Entry(entity).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
