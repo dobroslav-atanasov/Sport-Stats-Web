c6301ff75ec32670acc5a461809bdb086093a835
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-29 18:07:24 +0200

    Cache models were created.

---
+++ src/Data/SportStats.Data.Models/Cache/OGVenueCacheModel.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Data.Models.Cache;
+
+public class OGVenueCacheModel
+{
+    public int Id { get; set; }
+
+    public int Number { get; set; }
+}
---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGEventVenue.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("OlympicGames_EventsVenues", Schema = "dbo")]
+public class OGEventVenue
+{
+    public int EventId { get; set; }
+    public virtual OGEvent Event { get; set; }
+
+    public int VenueId { get; set; }
+    public virtual OGVenue Venue { get; set; }
+}
