745f91e47978e530eeebdf9b00d81fba9bbcc85b
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-11-30 08:33:24 +0200

    A service was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/EventVenueService.cs
@@ -0,0 +1,27 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+public class EventVenueService : BaseSportStatsService, IEventVenueService
+{
+    public EventVenueService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public bool EventVenueExists(int eventId, int venueId)
+    {
+        return this.Context.OGEventVenues.Any(x => x.EventId == eventId && x.VenueId == venueId);
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IEventVenueService.cs
@@ -0,0 +1,6 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+public interface IEventVenueService : IAddable
+{
+    bool EventVenueExists(int eventId, int venueId);
+}
