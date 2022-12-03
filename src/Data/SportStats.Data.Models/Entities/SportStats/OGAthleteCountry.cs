7b54cfe8a6f39751e2570cf22e37713dee0756fe
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-03 15:07:14 +0200

    THe map entity was created.

---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGAthleteCountry.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("OlympicGames_AthletesCountries", Schema = "dbo")]
+public class OGAthleteCountry
+{
+    public int AthleteId { get; set; }
+    public virtual OGAthlete Athlete { get; set; }
+
+    public int CountryId { get; set; }
+    public virtual OGCountry Country { get; set; }
+}
