e7ce343b911bc3db1b8698350f1fc457f54e6a57
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-03 15:08:25 +0200

    The AthletesService was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/AthletesService.cs
@@ -0,0 +1,42 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class AthletesService : BaseSportStatsService, IAthletesService
+{
+    public AthletesService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<OGAthlete> GetAthleteByNumberAsync(int number)
+    {
+        var athlete = await this.Context
+            .OGAthletes
+            .FirstOrDefaultAsync(a => a.Number == number);
+
+        return athlete;
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        this.Context.Entry(entity).State = EntityState.Modified;
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IAthletesService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface IAthletesService : IAddable, IUpdatable
+{
+    Task<OGAthlete> GetAthleteByNumberAsync(int number);
+}
