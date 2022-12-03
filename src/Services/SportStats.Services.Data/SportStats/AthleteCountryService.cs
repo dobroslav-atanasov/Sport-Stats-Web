0fc4bf2004972ab715d440a163a28ba52ba010ae
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-03 15:07:54 +0200

    The service and interface were introduced.

---
+++ src/Services/SportStats.Services.Data/SportStats/AthleteCountryService.cs
@@ -0,0 +1,27 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+public class AthleteCountryService : BaseSportStatsService, IAthleteCountryService
+{
+    public AthleteCountryService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        await this.Context.AddAsync(entity);
+        await this.Context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public bool AthleteCountryExists(int athleteId, int countryId)
+    {
+        return this.Context.OGAthleteCountries.Any(x => x.AthleteId == athleteId && x.CountryId == countryId);
+    }
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IAthleteCountryService.cs
@@ -0,0 +1,6 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+public interface IAthleteCountryService : IAddable
+{
+    bool AthleteCountryExists(int athleteId, int countryId);
+}
