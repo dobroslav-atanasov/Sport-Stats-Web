55ce9939120cbaf03b193af868a6a59a58d37dc0
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-07 15:22:32 +0200

    The converter ParticipantConverter was created.

---
+++ src/Common/SportStats.Common.Converters/Olympedia/ParticipantConverter.cs
@@ -0,0 +1,36 @@
+﻿namespace SportStats.Common.Converters.Olympedia;
+
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Logging;
+
+using SportStats.Data.Models.Entities.Crawlers;
+using SportStats.Services.Data.CrawlerStorage.Interfaces;
+using SportStats.Services.Data.SportStats.Interfaces;
+using SportStats.Services.Interfaces;
+
+public class ParticipantConverter : BaseOlympediaConverter
+{
+    public ParticipantConverter(ILogger<BaseConverter> logger, ICrawlersService crawlersService, ILogsService logsService, IGroupsService groupsService, IZipService zipService,
+        IRegexService regexService, IDataCacheService dataCacheService, INormalizeService normalizeService)
+        : base(logger, crawlersService, logsService, groupsService, zipService, regexService, dataCacheService, normalizeService)
+    {
+    }
+
+    protected override async Task ProcessGroupAsync(Group group)
+    {
+        try
+        {
+            var document = this.CreateHtmlDocument(group.Documents.Single(x => x.Order == 1));
+            var originalEventName = document.DocumentNode.SelectSingleNode("//ol[@class='breadcrumb']/li[@class='active']").InnerText;
+            var ogGameCache = this.FindGame(document);
+            var ogDisciplineCache = this.FindDiscipline(document);
+            var eventModel = this.CreateEventModel(originalEventName, ogGameCache, ogDisciplineCache);
+            ;
+        }
+        catch (Exception ex)
+        {
+            this.Logger.LogError(ex, $"Failed to process group: {group.Identifier}");
+        }
+    }
+}
