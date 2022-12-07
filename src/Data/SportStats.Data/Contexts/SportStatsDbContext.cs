c99d330b78392c986ce47a5ddc38f36ba8cb7a48
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-07 15:28:00 +0200

    Db sets were added.

--- src/Data/SportStats.Data/Contexts/SportStatsDbContext.cs
+++ src/Data/SportStats.Data/Contexts/SportStatsDbContext.cs
@@ -17,4 +17,29 @@ public class SportStatsDbContext : IdentityDbContext
     public virtual DbSet<OGCountry> OGCountries { get; set; }
 
     public virtual DbSet<OGGame> OGGames { get; set; }
+
+    public virtual DbSet<OGSport> OGSports { get; set; }
+
+    public virtual DbSet<OGDiscipline> OGDisciplines { get; set; }
+
+    public virtual DbSet<OGVenue> OGVenues { get; set; }
+
+    public virtual DbSet<OGEvent> OGEvents { get; set; }
+
+    public virtual DbSet<OGEventVenue> OGEventVenues { get; set; }
+
+    public virtual DbSet<OGAthlete> OGAthletes { get; set; }
+
+    public virtual DbSet<OGAthleteCountry> OGAthleteCountries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<OGEventVenue>()
+            .HasKey(ev => new { ev.EventId, ev.VenueId });
+
+        modelBuilder.Entity<OGAthleteCountry>()
+            .HasKey(ac => new { ac.AthleteId, ac.CountryId });
+
+        base.OnModelCreating(modelBuilder);
+    }
 }
