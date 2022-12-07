acb52be12a203aa39432809d620902981453dd08
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-07 15:25:08 +0200

    File with constants was updated.

--- src/Common/SportStats.Common.Constants/ConverterConstants.cs
+++ src/Common/SportStats.Common.Constants/ConverterConstants.cs
@@ -9,4 +9,5 @@ public static class ConverterConstants
     public const string OLYMPEDIA_SPORT_DISCIPLINE_CONVERTER = "SportStats.Common.Crawlers.Olympedia.SportDisciplineCrawler";
     public const string OLYMPEDIA_VENUE_CONVERTER = "SportStats.Common.Crawlers.Olympedia.VenueCrawler";
     public const string OLYMPEDIA_RESULT_CONVERTER = "SportStats.Common.Crawlers.Olympedia.ResultCrawler";
+    public const string OLYMPEDIA_ATHELETE_CONVERTER = "SportStats.Common.Crawlers.Olympedia.AthleteCrawler";
 }
