34a4dce8d374d21f98c3c96dbaccf2933fb434d2
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-09 11:40:25 +0200

    Cache model was added.

---
+++ src/Data/SportStats.Data.Models/Cache/OlympicGames/CountryCacheModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SportStats.Data.Models.Cache.OlympicGames;
+
+public class CountryCacheModel
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Code { get; set; }
+}
