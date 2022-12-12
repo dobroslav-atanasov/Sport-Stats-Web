3897a5ab27a9816bae377404d84bbdec4cb3219e
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-12 14:36:46 +0200

    The model was updated.

--- src/Data/SportStats.Data.Models/Cache/OlympicGames/CountryCacheModel.cs
+++ src/Data/SportStats.Data.Models/Cache/OlympicGames/CountryCacheModel.cs
@@ -1,10 +1,21 @@
 ﻿namespace SportStats.Data.Models.Cache.OlympicGames;
 
-public class CountryCacheModel
+using AutoMapper;
+
+using SportStats.Data.Models.Entities.SportStats;
+using SportStats.Services.Mapper.Interfaces;
+
+public class CountryCacheModel : IMapFrom<OGCountry>, ICustomMap
 {
     public int Id { get; set; }
 
     public string Name { get; set; }
 
     public string Code { get; set; }
+
+    public void CreateMap(IProfileExpression profileExpression)
+    {
+        profileExpression.CreateMap<OGCountry, CountryCacheModel>()
+            .ForMember(x => x.Name, options => options.MapFrom(x => x.CountryName));
+    }
 }
