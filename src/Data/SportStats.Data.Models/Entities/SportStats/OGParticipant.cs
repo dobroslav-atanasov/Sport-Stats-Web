bbd9a43b6e116d8d8a3058ccda7867ec3fcd2e91
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-16 08:54:45 +0200

    The entity was added.

---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGParticipant.cs
@@ -0,0 +1,49 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportStats.Data.Models.Entities.Interfaces;
+
+[Table("OlympicGames_Participants", Schema = "dbo")]
+public class OGParticipant : BaseEntity<int>, IUpdatable<OGParticipant>
+{
+    public int AthleteId { get; set; }
+    public virtual OGAthlete Athlete { get; set; }
+
+    public int EventId { get; set; }
+    public virtual OGEvent Event { get; set; }
+
+    public string Country { get; set; }
+
+    public int? AgeYears { get; set; }
+
+    public int? AgeDays { get; set; }
+
+    [NotMapped]
+    public int OlympediaNumber { get; set; }
+
+    public bool Update(OGParticipant other)
+    {
+        var isUpdated = false;
+
+        if (this.Country != other.Country)
+        {
+            this.Country = other.Country;
+            isUpdated = true;
+        }
+
+        if (this.AgeYears != other.AgeYears)
+        {
+            this.AgeYears = other.AgeYears;
+            isUpdated = true;
+        }
+
+        if (this.AgeDays != other.AgeDays)
+        {
+            this.AgeDays = other.AgeDays;
+            isUpdated = true;
+        }
+
+        return isUpdated;
+    }
+}
