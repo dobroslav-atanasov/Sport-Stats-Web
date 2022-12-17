0db054903e68c54352647c402e42f40b0f597c9f
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-17 12:55:57 +0200

    The participants service was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IParticipantsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface IParticipantsService : IAddable, IUpdatable
+{
+    Task<OGParticipant> GetParticipantAsync(int athleteId, int eventId);
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/ParticipantsService.cs
@@ -0,0 +1,30 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+public class ParticipantsService : BaseSportStatsService, IParticipantsService
+{
+    public ParticipantsService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<OGParticipant> GetParticipantAsync(int athleteId, int eventId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
