2593ab58c1dd55e057bc06fa96c5e93747cef8ff
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-18 13:44:01 +0200

    The entity was added.

---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGSquad.cs
@@ -0,0 +1,13 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("OlympicGames_Squads", Schema = "dbo")]
+public class OGSquad
+{
+    public int TeamId { get; set; }
+    public virtual OGTeam Team { get; set; }
+
+    public int ParticipantId { get; set; }
+    public virtual OGParticipant Participant { get; set; }
+}
