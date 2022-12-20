44b42ac211833daaca3cbf8d90c88edace015857
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-20 09:14:19 +0200

    THe service was modified.

--- src/Services/SportStats.Services.Data/SportStats/Interfaces/IVenuesService.cs
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/IVenuesService.cs
@@ -1,11 +1,11 @@
 ﻿namespace SportStats.Services.Data.SportStats.Interfaces;
 
-using global::SportStats.Data.Models.Cache;
+using global::SportStats.Data.Models.Cache.OlympicGames;
 using global::SportStats.Data.Models.Entities.SportStats;
 
 public interface IVenuesService : IAddable, IUpdatable
 {
     Task<OGVenue> GetVenueAsync(int number);
 
-    ICollection<OGVenueCacheModel> GetOGVenuesCache();
+    ICollection<VenueCacheModel> GetVenueCacheModels();
 }
