8c6436f9b03cac4f74b13171b5b585dc3afb9d90
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-21 08:46:06 +0200

    The squads service was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/ISquadsService.cs
@@ -0,0 +1,6 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+public interface ISquadsService : IAddable, IUpdatable
+{
+    bool SquadExists(int participantId, int teamId);
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/SquadsService.cs
@@ -0,0 +1,29 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+public class SquadsService : BaseSportStatsService, ISquadsService
+{
+    public SquadsService(SportStatsDbContext context)
+        : base(context)
+    {
+    }
+
+    public Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool SquadExists(int participantId, int teamId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
+}
