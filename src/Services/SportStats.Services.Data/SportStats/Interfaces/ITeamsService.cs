50b3202cbe9d095456edb1dbcda42fd3c12fd81c
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-22 08:40:58 +0200

    The service was added.

---
+++ src/Services/SportStats.Services.Data/SportStats/Interfaces/ITeamsService.cs
@@ -0,0 +1,8 @@
+﻿namespace SportStats.Services.Data.SportStats.Interfaces;
+
+using global::SportStats.Data.Models.Entities.SportStats;
+
+public interface ITeamsService : IAddable, IUpdatable
+{
+    Task<OGTeam> GetTeamAsync(string name, int eventId, int countryId);
+}
---
+++ src/Services/SportStats.Services.Data/SportStats/TeamsService.cs
@@ -0,0 +1,52 @@
+﻿namespace SportStats.Services.Data.SportStats;
+
+using System.Threading.Tasks;
+
+using global::SportStats.Data.Contexts;
+using global::SportStats.Data.Factory.Interfaces;
+using global::SportStats.Data.Models.Entities.SportStats;
+using global::SportStats.Services.Data.SportStats.Interfaces;
+
+using Microsoft.EntityFrameworkCore;
+
+public class TeamsService : BaseSportStatsService, ITeamsService
+{
+    private readonly IDbContextFactory dbContextFactory;
+
+    public TeamsService(SportStatsDbContext context, IDbContextFactory dbContextFactory)
+        : base(context)
+    {
+        this.dbContextFactory = dbContextFactory;
+    }
+
+    public async Task<TEntity> AddAsync<TEntity>(TEntity entity)
+    {
+        using var context = this.dbContextFactory.CreateSportStatsDbContext();
+
+        await context.AddAsync(entity);
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
+
+    public async Task<OGTeam> GetTeamAsync(string name, int eventId, int countryId)
+    {
+        using var context = this.dbContextFactory.CreateSportStatsDbContext();
+
+        var team = await context
+            .OGTeams
+            .FirstOrDefaultAsync(t => t.Name == name && t.EventId == eventId && t.CountryId == countryId);
+
+        return team;
+    }
+
+    public async Task<TEntity> UpdateAsync<TEntity>(TEntity entity)
+    {
+        using var context = this.dbContextFactory.CreateSportStatsDbContext();
+
+        context.Entry(entity).State = EntityState.Modified;
+        await context.SaveChangesAsync();
+
+        return entity;
+    }
+}
