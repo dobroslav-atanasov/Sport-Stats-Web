46f9a4ad9fcc682f46e2307d88ecd50b7448f038
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-27 14:21:40 +0200

    An entity was added.

---
+++ src/Data/SportStats.Data.Models/Entities/SportStats/OGStanding.cs
@@ -0,0 +1,65 @@
+﻿namespace SportStats.Data.Models.Entities.SportStats;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+using global::SportStats.Data.Models.Entities.Interfaces;
+using global::SportStats.Data.Models.Enumerations;
+
+[Table("OlympicGames_Standings", Schema = "dbo")]
+public class OGStanding : BaseEntity<int>, ICreatableEntity, IDeletableEntity, IUpdatable<OGStanding>
+{
+    [Required]
+    public int EventId { get; set; }
+    public virtual OGEvent Event { get; set; }
+
+    [Required]
+    public bool IsTeamEvent { get; set; } = false;
+
+    public int? ParticipantId { get; set; }
+    public virtual OGParticipant Participant { get; set; }
+
+    public int? TeamId { get; set; }
+    public virtual OGTeam Team { get; set; }
+
+    public int? Rank { get; set; }
+
+    [Required]
+    public MedalType Medal { get; set; }
+
+    [Required]
+    public FinishStatus FinishStatus { get; set; }
+
+    public DateTime CreatedOn { get; set; }
+
+    public DateTime? ModifiedOn { get; set; }
+
+    public bool IsDeleted { get; set; }
+
+    public DateTime? DeletedOn { get; set; }
+
+    public bool Update(OGStanding other)
+    {
+        var isUpdated = false;
+
+        if (this.Rank != other.Rank)
+        {
+            this.Rank = other.Rank;
+            isUpdated = true;
+        }
+
+        if (this.Medal != other.Medal)
+        {
+            this.Medal = other.Medal;
+            isUpdated = true;
+        }
+
+        if (this.FinishStatus != other.FinishStatus)
+        {
+            this.FinishStatus = other.FinishStatus;
+            isUpdated = true;
+        }
+
+        return isUpdated;
+    }
+}
