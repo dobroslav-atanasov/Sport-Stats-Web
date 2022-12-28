011023404f541fd6300bc18206828c077a77394a
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2022-12-28 12:49:06 +0200

    Enum was added.

---
+++ src/Data/SportStats.Data.Models/Enumerations/FinishStatus.cs
@@ -0,0 +1,11 @@
+﻿namespace SportStats.Data.Models.Enumerations;
+
+public enum FinishStatus
+{
+    None = 0,
+    Finish = 1,
+    DidNotFinish = 2,
+    Disqualified = 3,
+    AlsoCompeted = 4,
+    DidNotStart = 5,
+}
