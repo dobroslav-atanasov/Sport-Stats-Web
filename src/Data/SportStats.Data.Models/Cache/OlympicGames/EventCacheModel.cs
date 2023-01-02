21d1f6e116fe9214753726b7cff3611b67d4dd7c
Dobroslav Atanasov <dobroslav.l.atanasov@gmail.com>
2023-01-02 13:41:53 +0200

    The cache model was introduced.

--- src/Data/SportStats.Data.Models/Cache/OlympicGames/EventCacheModel.cs
+++ src/Data/SportStats.Data.Models/Cache/OlympicGames/EventCacheModel.cs
@@ -1,8 +1,21 @@
 ﻿namespace SportStats.Data.Models.Cache.OlympicGames;
 
-public class EventCacheModel
+using SportStats.Data.Models.Entities.SportStats;
+using SportStats.Services.Mapper.Interfaces;
+
+public class EventCacheModel : IMapFrom<OGEvent>
 {
     public int Id { get; set; }
 
-    public string MyProperty { get; set; }
+    public string OriginalName { get; set; }
+
+    public string Name { get; set; }
+
+    public string NormalizedName { get; set; }
+
+    public bool IsTeamEvent { get; set; }
+
+    public int DisciplineId { get; set; }
+
+    public int GameId { get; set; }
 }
